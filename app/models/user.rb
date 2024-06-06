5bd9fb032ad2f269d977afd82a81dd408766321b
Thomas NACHAR <thomas.nachar18@gmail.com>
2024-06-06 22:45:09 +0200

    Création du modèle utilisateur

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :telephone, presence: true
+
+end
