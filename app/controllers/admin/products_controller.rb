a3b12eac07e880a05d51c36f5840cc0cf0e16b90
Thomas NACHAR <thomas.nachar18@gmail.com>
2024-06-13 19:33:15 +0200

    Formulaire de création de produits

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,12 +1,24 @@
 class Admin::ProductsController < ApplicationController
   def index
-
+    @products = Product.all
   end
   def new
     @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      @products = Product.all
+      render :index
+    else
+      add_notification(@product.errors.full_messages, "error")
+    end
+  end
 
+  private
+
+  def product_params
+    params.require(:product).permit(:label, :sku, :category_id, :quantity, :price, :description)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,5 @@ class Product < ApplicationRecord
   extend FriendlyId
   friendly_id :label, use: :slugged
 
-  has_one_attached :image
   belongs_to :category
 end
