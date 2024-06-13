20090953b41f451acc2ea03431d4ffa36d6be795
Thomas NACHAR <thomas.nachar18@gmail.com>
2024-06-13 20:39:23 +0200

    Logique de modification et de suppression des produits

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -16,6 +16,27 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.friendly.find(params[:slug])
+  end
+
+  def update
+    @product = Product.friendly.find(params[:slug])
+    if @product.update(product_params)
+      @products = Product.all
+      render :index
+    else
+      add_notification(@product.errors.full_messages, "error")
+    end
+  end
+
+  def destroy
+    @product = Product.friendly.find(params[:slug])
+    @product.destroy
+    @products = Product.all
+    render :index
+  end
+
   private
 
   def product_params
